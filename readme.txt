3c23ddd296a8a63d115df02a4a6fa9b64d33aed1
Alexey Tataurov <alexey.tataurov@gmail.com>
2022-10-04 22:14:07 +0600

    Initial commit.

---
+++ readme.txt
@@ -0,0 +1,6 @@
+Create a website:
+- user login/logout
+- user roles
+- fetch data from a remote web-service
+- dashboard page that shows fetched data
+- publish to docker
---
+++ resources/views/layouts/partials/navbar.blade.php
@@ -0,0 +1,23 @@
+<header class="p-3 bg-dark text-white">
+  <div class="container">
+    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="" class="nav-link px-2 text-secondary">Home</a></li>
+      </ul>
+
+      @auth
+        <span class="me-3">Welcome, {{auth()->user()->username}}</span>
+        <div class="text-end">
+          <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-2">Logout</a>
+        </div>
+      @endauth
+
+      @guest
+        <div class="text-end">
+          <a href="{{ route('login.perform') }}" class="btn btn-outline-light me-2">Login</a>
+          <a href="{{ route('register.perform') }}" class="btn btn-warning">Sign-up</a>
+        </div>
+      @endguest
+    </div>
+  </div>
+</header>
